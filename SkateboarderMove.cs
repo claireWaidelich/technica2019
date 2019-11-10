189cd4b3a091aba6feb7ea07a8e8936b1079783d
claireWaidelich <claire.waidelich@emu.edu>
2019-11-10 11:31:26 -0500

    Add files via upload

---
+++ SkateboarderMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkateboarderMove : MonoBehaviour
+{
+    public bool boolean;
+    public int num;
+    public int[] arr;
+    public Color color;
+    private RectTransform rect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+    {
+        rect.
+    }
+}
